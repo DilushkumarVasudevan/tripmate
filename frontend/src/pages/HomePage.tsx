c728af0ae005e57802466cc1e099438fc6f3c6c5
Dilushkumar Vasudevan <dilushvasu1996@gmail.com>
2026-06-22 09:46:31 +0530

    Hero Sectio Creation

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "../shared/components/HeroSection";
 import Navbar from "../shared/components/Navbar";
 
 export default function HomePage() {
   return (
+    <>
     <Navbar />
+    <HeroSection />
+    </>
   );
 }
---
+++ frontend/src/shared/components/HeroSection.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import heroImage from "../../assets/images/HeroImage.png";
+import './HeroSection.css'
+
+export default function HeroSection() {
+
+    return (
+
+        <section className="hero-section">
+            <div className="hero-content">
+                <h1>
+                    Plan Trips Together.
+                    <br />
+                    Better. <span>Smarter.</span> Easier.
+                </h1>
+                <p>AI-powered collaborative travel planning platform for unforgettable journeys. </p>
+            <div className="hero-actions">
+                <Link to="/login" className="btn">
+                    Get Started
+                </Link>
+
+                <Link to="/register" className="btn btn-secondary">
+                    Sign Up
+                </Link>
+            </div>
+            </div>
+            <div className="hero-image">
+                <img src={heroImage} alt="Friends planning a trip together using TripMate" />
+            </div>
+        </section>
+
+    )
+}
