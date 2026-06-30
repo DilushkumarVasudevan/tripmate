712572bdcb13762209006fa7810450d80e44f697
Dilushkumar Vasudevan <dilushvasu1996@gmail.com>
2026-06-30 19:11:37 +0530

    moved the HomePage compnented to realvat folders

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import HeroSection from "../shared/components/HeroSection";
-import Navbar from "../shared/components/Navbar";
+import HeroSection from "../shared/components/HeroSection/HeroSection";
+import Navbar from "../shared/components/Navbar/Navbar";
 
 export default function HomePage() {
   return (
